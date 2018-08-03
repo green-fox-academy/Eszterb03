57850afa42b5270957727238e750d16397f628d0
Eszterb03 <eszter.b03@gmail.com>
2018-08-03 14:39:37 +0200

    pyramid is done

--- Week-01/day-4/pyramid.ts
+++ Week-01/day-4/pyramid.ts
@@ -5,21 +5,26 @@
 // Write a program that draws a
 // pyramid like this:
 //
-//    *
 //   ***
 //  *****
 // *******
 //
+//    *
 // The pyramid should have as many lines as lineCount is
 // for loop amiben van egy space loop (csökkenteni) és egy csillag loop(növelni)
 // ki kell íratni vele 3 spacet és csökkenteni
 
 let lineCount: number = 4;
-let spaceCount: string = '';
-let line2: string = '';
+let line2: string = '*';
 
 
-for (let j=0; j
- (let j=0 ; j <= 4; j++);
-{   line2 = line2 + '*'
-    console.log(line2); }
+for (let j: number = 0; j <= lineCount; j++) {
+    let spaceCount: string = '';
+    for (let k: number = j; k <= (lineCount - 1); k++) {
+        spaceCount += ' ';
+    }
+    line2 += '**';
+    console.log(spaceCount + line2);
+
+}
+

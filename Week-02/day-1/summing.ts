c4659b8228086931426559ab0852b38ca46aad15
Eszterb03 <eszter.b03@gmail.com>
2018-08-06 16:10:58 +0200

    Function sum with loop

---
+++ Week-02/day-1/summing.ts
@@ -0,0 +1,12 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+let givenNumber = 6
+
+function sum(givenNumber: number) {
+    let total = 0
+    for (let i = 0; i <= givenNumber; i++){
+        total = total + i}
+        return total;
+}
+    
+console.log(sum(givenNumber))

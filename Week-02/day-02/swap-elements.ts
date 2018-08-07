3a8d0f5dc6bc683dfeb031641ffaeafeb135dc92
Eszterb03 <eszter.b03@gmail.com>
2018-08-07 13:56:53 +0200

    half of the data structure

---
+++ Week-02/day-02/swap-elements.ts
@@ -0,0 +1,13 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+
+let abc = ["Arthur", "Boe", "Chole"];
+
+//console.log(abc[2], abc[1], abc[0]);
+
+let tempBox = abc[0]
+let tempBox2= abc[2]
+
+console.log(tempBox2,abc[1],tempBox);
+
---
+++ Week-02/day-02/test.ts
@@ -0,0 +1,20 @@
+/*let student1 = {
+    name:"Bela",
+    age: 20
+};
+
+let student2 = {
+    name:"Bela",
+    age: 20
+};
+*/
+
+let myClass:object[]= [
+    {name:"bela",
+    age: 20
+},{
+    name:"Anna",
+    age: 23
+}
+];
+console.log(myClass[1]["age"])

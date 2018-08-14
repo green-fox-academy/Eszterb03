9fc767ccd708264a3b1c837fb7bea23e92a038f7
Eszterb03 <eszter.b03@gmail.com>
2018-08-14 17:03:26 +0200

    (feat): copy content from file to another one

---
+++ Week-03/day-02/copy-file.ts
@@ -0,0 +1,33 @@
+// Write a function that copies a file to an other
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+'use srtict';
+​
+export {};
+​
+declare function require(path:string): any;
+const fs = require('fs');
+​const charEndcoding = 'utf-8'
+
+
+const fileWrite = (path, content) => {
+    try {
+     fs.writeFileSync(path, content);
+    } catch (error) {
+     console.log(error.message);
+    }
+   }
+ fileWrite('newfilefrom.txt','copy this text')
+ fileWrite('newfileto.txt','empty now')
+
+   function copier( fileFrom:string, fileTo:string):boolean {
+       try{ 
+           let content : string = fs.readFileSync(fileFrom,charEndcoding)
+           fileWrite(fileTo,content)
+       }
+       catch(error){
+           return false
+       }
+       return true
+   }
+   copier('newfilefrom.txt','newfileto.txt')

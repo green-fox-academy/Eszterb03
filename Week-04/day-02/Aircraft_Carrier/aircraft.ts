28d0b36a644882d299ae2454d2b7052ff2294cb8
Eszterb03 <eszter.b03@gmail.com>
2018-08-21 16:37:57 +0200

    add(methods)working function inside mother class

--- Week-04/day-02/Aircraft_Carrier/aircraft.ts
+++ Week-04/day-02/Aircraft_Carrier/aircraft.ts
@@ -1,46 +1,60 @@
-import { F35 } from "./F35";
-
 'use strict'
 
-export class Aircraft{
-  
+export class Aircraft {
+
   protected baseDamage: number;
   protected maxAmmo: number;
-  protected typeName : string;
+  protected typeName: string;
   protected ammo: number;
+  allDamage: number;
+  currentAmmo: number;
 
-  constructor(typeName:string){
-    // if( this.typeName !== (F16||F35)){
-    //   throw new Error( "This type isn't exist ");
-    // }
-    // if (this.typeName == F16){
-    //   this.maxAmmo == 8;
-    //   this.baseDamage == 30;
-    // }
-    // if(this.typeName == F35){
-    //   this.maxAmmo == 12;
-    //   this.baseDamage == 50;
-    // }
+  constructor(typeName: string, maxAmmo: number, baseDamage: number) {
 
     this.typeName = typeName;
     this.ammo = 0;
-   
-    
+    this.allDamage = 0;
+    this.baseDamage = baseDamage;
+    this.maxAmmo = maxAmmo;
+    this.currentAmmo = 0;
+ 
   }
 
-  fight(){
+  fight() {
     this.ammo = 0;
-    let damageOfFight = this.baseDamage*this.ammo
+    let damageOfFight = this.baseDamage * this.ammo
+    this.allDamage += damageOfFight;
     return damageOfFight
   }
-  refill(numberOfRefill:number){
-    if( this.ammo + numberOfRefill < this.maxAmmo && numberOfRefill < this.ammo ){
-     return this.maxAmmo - numberOfRefill
+  refill(numberOfRefill: number) {
+    
+    if (this.ammo + numberOfRefill < this.maxAmmo) {
+      this.currentAmmo += this.ammo + numberOfRefill;
+    return this.ammo + numberOfRefill
+    
+    
+    } else if (numberOfRefill + this.ammo > this.maxAmmo ) {
+      this.currentAmmo = this.maxAmmo
+      return this.maxAmmo
+    
     } else {
-      let sumOfAmmo = this.ammo+numberOfRefill
-      return sumOfAmmo-this.maxAmmo
+      let remainingOfAmmo = this.ammo + numberOfRefill;
+      return remainingOfAmmo - this.maxAmmo;
     }
   }
-
+getType(){
+  return (`${this.typeName}`)
 }
+getStatus(){
+  return (`${this.typeName}, Ammo: ${this.currentAmmo}, Base Damage:${this.baseDamage}, All Damage: ${this.allDamage}`)
+  }
+}
+
+let aircraft1: Aircraft = new Aircraft('F35', 12, 50);
+let aircraft2: Aircraft = new Aircraft('F16', 8, 30)
+
+console.log(aircraft1.getType(),aircraft2.getType())
+console.log(aircraft1.getStatus())
+console.log(aircraft1.refill(5))
+console.log(aircraft1.getStatus())
 

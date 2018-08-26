ebc7fb19f616b9a7ebab68c0ff05456a71968ef2
Eszterb03 <eszter.b03@gmail.com>
2018-08-26 17:07:27 +0200

    add(features) filter double char

---
+++ Week-03/day-02/duplicated-chars.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+import { readFileSync } from "fs";
+
+
+function duplicatedChars(fileName: string) {
+  let fileContent = readFileSync(fileName, 'utf-8');
+ return fileContent.split('\r\n').map(elem => {
+    let newLineContent = ''
+    for (let i: number = 0; i < elem.length; i++) {
+      if (i !%2 == 0){
+        newLineContent += elem.charAt(i);
+      }
+    }
+    return newLineContent;
+  }).join("\r\n");
+}
+
+console.log(duplicatedChars('duplicated-chars.txt'))

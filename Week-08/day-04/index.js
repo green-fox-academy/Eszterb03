a3d2b1d94a5dda2901028220b247845b358a8bd4
Eszterb03 <eszter.b03@gmail.com>
2018-09-27 15:59:08 +0200

    add(docs) login form with basic js

---
+++ Week-08/day-04/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.onload = () => {
+  const form = document.querySelector('#formHandler');
+  const cat = document.querySelector('#cat');
+  const dog = document.querySelector('#dog');
+
+  console.log(cat)
+
+}

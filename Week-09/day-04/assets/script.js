d92595691ddbaaaede211c4dc7dbb9cdc805c21c
Eszterb03 <eszter.b03@gmail.com>
2018-10-03 19:12:38 +0200

    add(question endpoint) interval function wait 5 mp

--- Week-09/day-04/assets/script.js
+++ Week-09/day-04/assets/script.js
@@ -1,12 +1,16 @@
 
 window.onload = () => {
   const url = 'http://localhost:3030/questions'
+  let question = document.querySelector('h3');
 
   fetch(url)
     .then((res) => res.json())
     .then((res) => {
-      console.log(res)
-      let question = document.querySelector('h3');
-      question.innerHTML = res.quiz[0].question
+      question.innerText = res.quiz[0].question
+      let i = 1;
+      setInterval(() => {
+        question.innerText = res.quiz[i].question,
+          i++
+      }, 3000)
     })
 }

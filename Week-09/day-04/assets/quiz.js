828590e247f6701bc1ed92e42a620c4eb5e2d47d
Eszterb03 <eszter.b03@gmail.com>
2018-10-04 14:23:20 +0200

    add(docs): post endpoint connect with form

---
+++ Week-09/day-04/assets/quiz.js
@@ -0,0 +1,43 @@
+window.onload = () => {
+  const form = document.querySelector('#formquiz')
+
+
+  // form.addEventListener('submit', (event) => {
+  // event.preventDefault();
+  // const { question, answer1, answer2, answer3, answer4, } = event.target.elements;
+
+  // let radioBtnBoolean = [];
+  // for (let i = 0; i < event.target.elements.is_correct.length; i++) {
+  //   results.push(event.target.elements.is_correct[i].checked);
+  // }
+
+  // console.log(results)
+  // const bodyToSend = {
+  //   tips: results
+  // }
+
+  // const body = { name: question.value, answer1: answer1.value, answer2: answer2.value, answer3: answer3.value, answer4: answer4.value gender: selected };
+
+  const URL = 'http://localhost:3030/questions';
+
+  // fetch(URL, {
+  //   method: 'post',
+  //   headers: {
+  //     'Content-Type': 'application/json; charset=utf-8'
+  //   },
+  //   redirect: 'error',
+  //   mode: 'cors',
+  //   body: JSON.stringify(bodyToSend)
+  // })
+  //   .then(resp => resp.json())
+
+  //   .catch(respErr => {
+  //     console.log('got no resp', respErr);
+  //   })
+  //   .then(response => {
+  //     document.querySelector('#res').innerHTML = response.answer;
+  //   })
+  //   .catch(error => {
+  //     console.log('hiba', error);
+  //   })
+} 
--- Week-09/day-04/assets/script.js
+++ Week-09/day-04/assets/script.js
@@ -21,13 +21,11 @@ window.onload = () => {
     fetch(url)
       .then((res) => res.json())
       .then((res) => {
-        let i = 0;
         question.innerText = res.question;
         answer1.innerText = res.answer[0].answer;
         answer2.innerText = res.answer[1].answer;
         answer3.innerText = res.answer[2].answer;
         answer4.innerText = res.answer[3].answer;
-        i++
       })
   }, 3000)
 }

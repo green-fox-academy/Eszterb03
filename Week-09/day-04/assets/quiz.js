21354d3fa4eeaf8b24c384bc7392f73bb4fe2916
Eszterb03 <eszter.b03@gmail.com>
2018-10-04 14:42:21 +0200

    add(question) to my manage div

--- Week-09/day-04/assets/quiz.js
+++ Week-09/day-04/assets/quiz.js
@@ -1,7 +1,17 @@
 window.onload = () => {
-  const form = document.querySelector('#formquiz')
+  const URL = 'http://localhost:3030/questions'
+  const manage = document.querySelector('.manage')
 
-
+  fetch(URL)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res)
+      res.quiz.forEach(element => {
+        const question = document.createElement('p')
+        manage.appendChild(question)
+        question.innerText = element.question
+      });
+    })
   // form.addEventListener('submit', (event) => {
   // event.preventDefault();
   // const { question, answer1, answer2, answer3, answer4, } = event.target.elements;
@@ -18,26 +28,27 @@ window.onload = () => {
 
   // const body = { name: question.value, answer1: answer1.value, answer2: answer2.value, answer3: answer3.value, answer4: answer4.value gender: selected };
 
-  const URL = 'http://localhost:3030/questions';
+  //   const URL = 'http://localhost:3030/questions';
 
-  // fetch(URL, {
-  //   method: 'post',
-  //   headers: {
-  //     'Content-Type': 'application/json; charset=utf-8'
-  //   },
-  //   redirect: 'error',
-  //   mode: 'cors',
-  //   body: JSON.stringify(bodyToSend)
-  // })
-  //   .then(resp => resp.json())
+  //   fetch(URL, {
+  //     method: 'post',
+  //     headers: {
+  //       'Content-Type': 'application/json; charset=utf-8'
+  //     },
+  //     redirect: 'error',
+  //     mode: 'cors',
+  //     body: JSON.stringify(bodyToSend)
+  //   })
+  //     .then(resp => resp.json())
 
-  //   .catch(respErr => {
-  //     console.log('got no resp', respErr);
-  //   })
-  //   .then(response => {
-  //     document.querySelector('#res').innerHTML = response.answer;
-  //   })
-  //   .catch(error => {
-  //     console.log('hiba', error);
-  //   })
+  //     .catch(respErr => {
+  //       console.log('got no resp', respErr);
+  //     })
+  //     .then(response => {
+  //       document.querySelector('#res').innerHTML = response.answer;
+  //     })
+  //     .catch(error => {
+  //       console.log('hiba', error);
+  //     })
+  // });
 } 
